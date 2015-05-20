f2f5dd6a836df67cba254c8dd7fa40de0d6a5107
Vinay Goel <vinaygo@gmail.com>
2015-05-20 15:59:57 -0700

    exercise-0 env

---
+++ setup-exercise-0-env.sh
@@ -0,0 +1,11 @@
+# Comment lines begin with a '#' symbol
+
+# Hadoop installation directory (change as needed)
+export HADOOP_INSTALL_DIR=/tmp/ars-hadoop-install
+
+export HADOOP_HOME=$HADOOP_INSTALL_DIR/hadoop-2.6.0
+
+# Exercise 0: Data directories (change as needed)
+export LOCAL_WARC_DIR=/tmp/ars-exercise-0-warcs
+export LOCAL_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
+export HDFS_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
