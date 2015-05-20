e70473fbcc939a77139c8064de4e0feef6dad0f5
Vinay Goel <vinaygo@gmail.com>
2015-05-20 16:04:16 -0700

    pig

--- bin/setup-pig.sh
+++ bin/setup-pig.sh
@@ -12,19 +12,9 @@ USER_NAME=`whoami`
 pig_version=pig-0.14.0
 pig_stable_mirror=http://apache.mirrors.hoobly.com/pig/$pig_version/$pig_version.tar.gz
 
-if [ -z "$JAVA_HOME" ]; then
-    echo "Please set JAVA_HOME"
-    exit 2
-fi
-
-if [ -z "$HADOOP_HOME" ]; then
-    echo "Please set HADOOP_HOME"
-    exit 3
-fi
-
 if [ -d "$INSTALL_DIR" ]; then
   echo "Please specify a non-existent installation directory: $INSTALL_DIR"
-  exit 4
+  exit 1
 fi
 
 mkdir -p $INSTALL_DIR
@@ -35,22 +25,4 @@ curl -O $pig_stable_mirror
 
 echo "Installing Pig"
 tar xfvz $pig_version.tar.gz
-export PIG_HOME=$INSTALL_DIR/$pig_version
-#export PATH=$PIG_HOME/bin:$PATH
-
-
-echo "Apache Pig has been set up"
-
-echo "Please set HADOOP_HOME to $HADOOP_HOME by running: "
-echo "export HADOOP_HOME=$HADOOP_HOME"
-
-
-echo "==========================================================="
-echo "Please set PIG_HOME to $PIG_HOME by running: "
-echo "export PIG_HOME=$PIG_HOME"
-echo ""
-echo "Please add Pig to your path by running: "
-echo "export PATH=$PIG_HOME/bin:\$PATH";
-echo "==========================================================="
-
-
+echo "Done."
