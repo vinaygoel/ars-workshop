a0f5694b82bf30f9f413bfe943bce53b9baa7bc4
Vinay Goel <vinaygo@gmail.com>
2015-05-21 18:30:44 -0700

    exercise-0 env

--- setup-exercise-0-env.sh
+++ setup-exercise-0-env.sh
@@ -1,11 +1,16 @@
 # Comment lines begin with a '#' symbol
 
-# Hadoop installation directory (change as needed)
+# Exercise-0
+
+# Hadoop pseudo mode and Pig installation directory (change as needed)
 export HADOOP_INSTALL_DIR=/tmp/ars-hadoop-install
+export PIG_INSTALL_DIR=/tmp/ars-pig-install
 
 export HADOOP_HOME=$HADOOP_INSTALL_DIR/hadoop-2.6.0
+export PIG_HOME=$PIG_INSTALL_DIR/pig-0.14.0
+export PATH=$PIG_HOME/bin:$PATH
 
-# Exercise 0: Data directories (change as needed)
+#Data directories (change as needed)
 export LOCAL_WARC_DIR=/tmp/ars-exercise-0-warcs
 export LOCAL_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
 export HDFS_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
