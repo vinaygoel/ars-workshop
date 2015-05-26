e4e52ab6c99f5c78b171c3194e6f75c77fd3c7a2
Vinay Goel <vinaygo@gmail.com>
2015-05-26 13:39:12 -0700

    exercise 0 env

--- setup-exercise-0-env.sh
+++ setup-exercise-0-env.sh
@@ -3,9 +3,10 @@
 # Exercise-0
 
 # Hadoop pseudo mode and Pig installation directory (change as needed)
-export HADOOP_INSTALL_DIR=/tmp/ars-hadoop-install
-export PIG_INSTALL_DIR=/tmp/ars-pig-install
+export PSEUDO_MODE_INSTALL_DIR=/tmp/
 
+export HADOOP_INSTALL_DIR=$PSEUDO_MODE_INSTALL_BASE_DIR/ars-hadoop-install
+export PIG_INSTALL_DIR=$PSEUDO_MODE_INSTALL_BASE_DIR/ars-pig-install
 export HADOOP_HOME=$HADOOP_INSTALL_DIR/hadoop-2.6.0
 export PIG_HOME=$PIG_INSTALL_DIR/pig-0.14.0
 export PATH=$PIG_HOME/bin:$PATH
