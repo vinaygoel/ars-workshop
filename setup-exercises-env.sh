57beb4e4d75e4a43dcf75ffc7ac0e264c61d702a
Vinay Goel <vinaygo@gmail.com>
2015-05-26 13:39:25 -0700

    exercises env

--- setup-exercises-env.sh
+++ setup-exercises-env.sh
@@ -1,10 +1,15 @@
 # Comment lines begin with a '#' symbol
 
-# Hadoop-2 / Pig and Elasticsearch local installation directories (change as needed)
-export HADOOP_PIG_LOCAL_INSTALL_DIR=/tmp/ars-hadoop-pig-local-install
-export ELASTICSEARCH_INSTALL_DIR=/tmp/ars-elasticsearch-install
+# installation and data directories (change as needed)
+export ARS_EXERCISES_INSTALL_DIR=/tmp/
+
+export ARS_DERIVATIVES_DIR=$ARS_EXERCISES_INSTALL_DIR/ars-exercises-derivatives
+
+export HADOOP_PIG_LOCAL_INSTALL_DIR=$ARS_EXERCISES_INSTALL_DIR/ars-hadoop-pig-local-install
+export ELASTICSEARCH_INSTALL_DIR=$ARS_EXERCISES_INSTALL_DIR/ars-elasticsearch-install
 export ELASTICSEARCH_HOME=$ELASTICSEARCH_INSTALL_DIR/elasticsearch-1.5.2
-export ARS_DERIVATIVES_DIR=/tmp/ars-exercises-derivatives
+export KIBANA_INSTALL_DIR=$ARS_EXERCISES_INSTALL_DIR/ars-kibana-install
+export KIBANA_HOME=$KIBANA_INSTALL_DIR/kibana-4.0.2
 
 export HADOOP_INSTALL_DIR=$HADOOP_PIG_LOCAL_INSTALL_DIR/hadoop-2-local-mode
 export HADOOP_HOME=$HADOOP_INSTALL_DIR/hadoop
