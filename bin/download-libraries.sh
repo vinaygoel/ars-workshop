116139330f3550f5506e8bd0d8673b161794bdff
Vinay Goel <vinaygo@gmail.com>
2015-05-26 20:37:37 -0700

    libraries

--- bin/download-libraries.sh
+++ bin/download-libraries.sh
@@ -3,7 +3,10 @@ set -e
 
 BIN_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LIB_DIR=$BIN_DIR/../lib/
+CACHE_DIR=$BIN_DIR/../cache/
 mkdir -p $LIB_DIR
+mkdir -p $CACHE_DIR
+
 downloadPath="https://archive.org/~vinay/archive-analysis/ia-libraries/hadoop-2.x/"
 libraryNames=(
        'webarchive-commons-jar-with-dependencies.jar'
@@ -31,6 +34,23 @@ for jar in ${libraryNames[@]}; do
    fi
 done
 
+cacheFileNames=(
+       'GeoLiteCity.dat'
+       'geo-pack.tgz'
+       'stop-words.txt'
+)       
+
+cd $CACHE_DIR
+
+#download
+for cacheFile in ${cacheFileNames[@]}; do
+   curl -O $downloadPath/$cacheFile
+   if [ $? -ne 0 ]; then
+      echo "Unable to download from $downloadPath/$cacheFile"
+      exit 1
+   fi
+done
+
 echo "==========================================================="
-echo "Libraries downloaded to $LIB_DIR"
+echo "Libraries downloaded to $LIB_DIR and $CACHE_DIR"
 echo "==========================================================="
