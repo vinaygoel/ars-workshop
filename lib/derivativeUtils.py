da1fce46dce7c4f0617ad58e4296dfd0436e18dd
Vinay Goel <vinaygo@gmail.com>
2015-05-28 19:36:24 -0700

    URL transform UDFs

--- lib/derivativeUtils.py
+++ lib/derivativeUtils.py
@@ -45,3 +45,39 @@ def getDateFormatFromWARCTS(timestamp):
    if not timestamp or len(timestamp) <= 14:
       return None
    return timestamp[0:10]
+
+@outputSchema("text:chararray")
+def getTLDFromDomain(domain):
+   if not domain:
+      return None
+   domainParts = domain.split('.')
+   domainParts.reverse()
+   tld = domainParts[0]
+   return tld
+
+
+@outputSchema("text:chararray")
+def getHostFromSurtUrl(surtUrl):
+   host = None
+   if not surtUrl:
+      return None
+   surtUrlParts = surtUrl.split('/')
+   surtHost = surtUrlParts[0]
+   #strip away any text after whitespace
+   surtHostParts = surtHost.split()
+   if len(surtHostParts) == 0:
+      return None
+   surtHost = surtHostParts[0]
+   #strip away port number
+   surtHostParts = surtHost.split(':')
+   surtHost = surtHostParts[0]
+   surtHost = remove_chars(surtHost)
+   surtHostParts = surtHost.split(',')
+   surtHostParts.reverse()
+   host = '.'.join(surtHostParts)
+   return host
+
+def remove_chars(to_translate, translate_to=u''):
+   chars_to_remove = u'()'
+   translate_table = dict((ord(char), translate_to) for char in chars_to_remove)
+   return to_translate.translate(translate_table)
