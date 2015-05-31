6cafdd3226bebb6a8ad2cbaea652c023ce611100
Vinay Goel <vinaygo@gmail.com>
2015-05-31 11:53:44 -0700

    exercise-0 env

--- setup-exercise-0-env.sh
+++ setup-exercise-0-env.sh
@@ -1,17 +1,19 @@
 # Comment lines begin with a '#' symbol
-
 # Exercise-0
 
-# Hadoop pseudo mode and Pig installation directory (change as needed)
-export PSEUDO_MODE_INSTALL_DIR=/tmp/
+# Hadoop pseudo mode install/data directory (change as needed)
+export PSEUDO_MODE_DIR=`pwd`/pseudo-mode
 
+export PSEUDO_MODE_INSTALL_DIR=$PSEUDO_MODE_DIR/install
+export PSEUDO_MODE_DATA_DIR=$PSEUDO_MODE_DIR/data
+
+export LOCAL_WARC_DIR=$PSEUDO_MODE_DATA_DIR/ars-exercise-0-warcs
+export LOCAL_DERIVATIVE_DIR=$PSEUDO_MODE_DATA_DIR/ars-exercise-0-derivatives
+export HDFS_DERIVATIVE_DIR=$PSEUDO_MODE_DATA_DIR/ars-exercise-0-derivatives
+
+#env variables
 export HADOOP_INSTALL_DIR=$PSEUDO_MODE_INSTALL_BASE_DIR/ars-hadoop-install
 export PIG_INSTALL_DIR=$PSEUDO_MODE_INSTALL_BASE_DIR/ars-pig-install
 export HADOOP_HOME=$HADOOP_INSTALL_DIR/hadoop-2.6.0
 export PIG_HOME=$PIG_INSTALL_DIR/pig-0.14.0
 export PATH=$PIG_HOME/bin:$PATH
-
-#Data directories (change as needed)
-export LOCAL_WARC_DIR=/tmp/ars-exercise-0-warcs
-export LOCAL_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
-export HDFS_DERIVATIVE_DIR=/tmp/ars-exercise-0-derivatives
