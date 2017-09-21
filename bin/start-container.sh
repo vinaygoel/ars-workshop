661116c3a70db0fe8ba28d94527d39149effa853
Vinay Goel <vinaygo@gmail.com>
2017-09-21 00:44:29 -0700

    add rm flag

--- bin/start-container.sh
+++ bin/start-container.sh
@@ -9,7 +9,7 @@ fi
 
 data_dir=$1
 
-docker run -i -t -p 9200:9200 -p 5601:5601 -p 8888:8888 \
+docker run --rm -i -t -p 9200:9200 -p 5601:5601 -p 8888:8888 \
        -v ${BASE_DIR}:/ars-workshop -v ${data_dir}:/ars-data \
        -v ${data_dir}/elasticsearch-index-data:/ars-elasticsearch-index-data \
        vinaygoel/ars-docker-notebooks
